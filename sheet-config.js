41b46d6731486ebfc9e6bbbe54ae7c1fe3b799ef
abuyamasts-sys <abuyamasts-sys@users.noreply.github.com>
2026-04-11 17:43:23 +0700

    Update Apps Script endpoint (doGet JSONP)

--- sheet-config.js
+++ sheet-config.js
@@ -1,6 +1,6 @@
 // Fill this after you deploy Apps Script Web App
 // Example: https://script.google.com/macros/s/XXXX/exec
-window.AIRTIS_SHEETS_ENDPOINT = "https://script.google.com/macros/s/AKfycbyxRQ7F6kGhCQ-3_VcfMXj3IK7CRLOsLX-Uv6EFgGr_lNNssJs79vejvB9Qmjvh6dMPwQ/exec";
+window.AIRTIS_SHEETS_ENDPOINT = "https://script.google.com/macros/s/AKfycbyb3S2FQRsiN4oobBQ9izcga5c25RrhWJtVdfOS9X4qIy97exX07gcPU-RGBu4uauP5gg/exec";
 
 // Optional shared secret (must match Apps Script). Leave empty to disable.
 window.AIRTIS_SHEETS_SECRET = "";
