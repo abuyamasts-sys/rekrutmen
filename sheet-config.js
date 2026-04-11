b41f7571d224a49b119394da466bbd273dea6aec
abuyamasts-sys <abuyamasts-sys@users.noreply.github.com>
2026-04-11 17:29:39 +0700

    Update Sheets endpoint URL

--- sheet-config.js
+++ sheet-config.js
@@ -1,6 +1,6 @@
 // Fill this after you deploy Apps Script Web App
 // Example: https://script.google.com/macros/s/XXXX/exec
-window.AIRTIS_SHEETS_ENDPOINT = "https://script.google.com/macros/s/AKfycbxUvmm_87Q_RMndelh5ERmfcU-hk-vLWY1GhvDi6GGL5StJE5w5G_BD5fJN-k_glJfPXA/exec";
+window.AIRTIS_SHEETS_ENDPOINT = "https://script.google.com/macros/s/AKfycbyxRQ7F6kGhCQ-3_VcfMXj3IK7CRLOsLX-Uv6EFgGr_lNNssJs79vejvB9Qmjvh6dMPwQ/exec";
 
 // Optional shared secret (must match Apps Script). Leave empty to disable.
 window.AIRTIS_SHEETS_SECRET = "";
